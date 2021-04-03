8c8b31b04d15572a4597e611bfa4f7e1d65bf30c
Gustavo Poppi <gustavopoppi@hotmail.com>
2021-04-03 19:32:43 -0300

    Refatoração de código

--- src/main/java/br/com/loja/mvc/sergio/comuns/StringExtensions.java
+++ src/main/java/br/com/loja/mvc/sergio/comuns/StringExtensions.java
@@ -2,34 +2,67 @@ package br.com.loja.mvc.sergio.comuns;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
 public class StringExtensions {
 
 	public static String incrementarMes(String dataIncrementar, int mesesAIncrementar) throws ParseException {
-		
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy"); //formato que eu quero ad ata
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy"); // formato que eu quero ad ata
 		Date data = dateFormat.parse(dataIncrementar); // conversão strig para Date
 		Calendar cal = Calendar.getInstance(); // pega a data de hoje
-		cal.setTime(data); // seta a hora para a que está na variável 
+		cal.setTime(data); // seta a hora para a que está na variável
 		cal.add(Calendar.MONTH, mesesAIncrementar);
-		
+
 		return dateFormat.format(cal.getTime()); // retornou a data incrementada em string
 	}
-	
+
 	public static String formatarDataVindoAoContrario(String data) throws ParseException {
 		SimpleDateFormat dateFormularioFormat = new SimpleDateFormat("yyyy-MM-dddd");
 		Date dataFormatada = dateFormularioFormat.parse(data);
-		
+
 		SimpleDateFormat dateFormatoBrasil = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		return dateFormatoBrasil.format(dataFormatada);
 	}
-	
+
 	public static String dataDeHoje() {
 		SimpleDateFormat dateFormatoBrasil = new SimpleDateFormat("dd/MM/yyyy");
 		Calendar cal = Calendar.getInstance();
-		return dateFormatoBrasil.format(cal.getTime());		
+		return dateFormatoBrasil.format(cal.getTime());
+	}
+
+	public static String retornaPrimeiroDiaMes() {
+		LocalDate dataAtual = LocalDate.now();
+
+		String PrimeiroDiaMes = String.format("%02d", (dataAtual.withDayOfMonth(1).getDayOfMonth()));
+		String mesAtual = retornaMesAtual();
+		String anoAtual = retornaAnoAtual();		
+
+		return anoAtual + '/' + mesAtual + '/' + PrimeiroDiaMes;
+	}
+
+	public static String retornaUltimoDiaMes() {
+		LocalDate dataAtual = LocalDate.now();
+		
+		String UltimoDiaMes = String.format("%02d",(dataAtual.withDayOfMonth(dataAtual.lengthOfMonth()).getDayOfMonth()));
+		String mesAtual = retornaMesAtual();
+		String anoAtual = retornaAnoAtual();
+		
+		return anoAtual + '/' + mesAtual + '/' + UltimoDiaMes;			
+	}
+	
+	private static String retornaMesAtual() {
+		LocalDate dataAtual = LocalDate.now();
+
+		return String.format("%02d", (dataAtual.getMonth().getValue()));
+	}
+	
+	private static String retornaAnoAtual() {
+		LocalDate dataAtual = LocalDate.now();
+
+		return String.format("%02d", (dataAtual.getYear()));
 	}
 }
--- src/main/java/br/com/loja/mvc/sergio/controller/HomeController.java
+++ src/main/java/br/com/loja/mvc/sergio/controller/HomeController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.loja.mvc.sergio.comuns.StringExtensions;
 import br.com.loja.mvc.sergio.model.Cliente;
 import br.com.loja.mvc.sergio.model.Parcela;
 import br.com.loja.mvc.sergio.model.Venda;
@@ -43,25 +44,9 @@ public class HomeController {
 
 	@GetMapping()
 	public String home(Model model){
-//		List<Pedido> pedidos = repository.findAll();
-//		model.addAttribute("pedidos", pedidos);
-
-		String dataPrimeiroDiaMes;
-		String dataUltimoDiaMes;
 		
-		LocalDate dataAtual = LocalDate.now();
-				
-		String PrimeiroDiaMes = String.format("%02d", (dataAtual.withDayOfMonth(1).getDayOfMonth()));
-		String mesAtual = String.format("%02d", (dataAtual.getMonth().getValue()));
-		String anoAtual = String.format("%02d", (dataAtual.getYear()));
-		String UltimoDiaMes = String.format("%02d", (dataAtual.withDayOfMonth(dataAtual.lengthOfMonth()).getDayOfMonth()));
-		
-		dataPrimeiroDiaMes = anoAtual + '/' + mesAtual + '/' + PrimeiroDiaMes;
-		dataUltimoDiaMes = anoAtual + '/' + mesAtual + '/' + UltimoDiaMes;
-		
-		SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd"); 
-//		Date data = formato.parse(dataPrimeiroDiaMes);
-//		Date data2 = formato.parse(dataUltimoDiaMes);
+		String dataPrimeiroDiaMes = StringExtensions.retornaPrimeiroDiaMes();
+		String dataUltimoDiaMes = StringExtensions.retornaUltimoDiaMes(); 
 		
 		List<Venda> consultaTeste = vendaRepository.consultaTeste(dataPrimeiroDiaMes, dataUltimoDiaMes);																
 		
@@ -71,8 +56,7 @@ public class HomeController {
 		List<Double> valoresTotaisClientesEmAberto = clienteRepository.findValorTotalClientesEmAberto(dataPrimeiroDiaMes, dataUltimoDiaMes);
 		model.addAttribute("valoresTotais", valoresTotaisClientesEmAberto);
 
-		List<Long> CountTotalPorClienteEmAberto = clienteRepository.findCountTotalPorClienteEmAberto(dataPrimeiroDiaMes, dataUltimoDiaMes);
-		int teste2 = CountTotalPorClienteEmAberto.size();
+		List<Long> CountTotalPorClienteEmAberto = clienteRepository.findCountTotalPorClienteEmAberto(dataPrimeiroDiaMes, dataUltimoDiaMes);		
 
 		List<Venda> vendas = vendaRepository.findAllByJoin(dataPrimeiroDiaMes, dataUltimoDiaMes); // falta ordenar
 		
@@ -81,14 +65,8 @@ public class HomeController {
 			valorTotalVendas += venda.getValorTotal();
 		}
 		model.addAttribute("valorTotalVendas", valorTotalVendas);
-//		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);
-//		model.addAttribute("vendas", vendas);
 
 		List<List<Venda>> listaVendasTeste = new ArrayList<>();
-
-//		int x = 0;
-//		while (x < CountTotalPorClienteEmAberto.size()) {
-
 		
 		int x = 0;
 		for (int i = 0; i < CountTotalPorClienteEmAberto.size(); i++) {
@@ -103,18 +81,14 @@ public class HomeController {
 					listaVendasTeste.add(listaTeste);
 				
 				x++;
-			}
-			
+			}			
 		}
-
 		model.addAttribute("vendas", listaVendasTeste);
 
 		List<Parcela> parcelas = parcelaRepository.findAllByJoin(dataPrimeiroDiaMes, dataUltimoDiaMes);
-		model.addAttribute("parcelas", parcelas);
+		model.addAttribute("parcelas", parcelas);		
 		
-		
-		model.addAttribute("localDate", LocalDate.now());
-		
+		model.addAttribute("localDate", LocalDate.now());		
 		
 		return "home";
 	}
