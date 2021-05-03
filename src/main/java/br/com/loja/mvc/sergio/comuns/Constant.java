ba75dd998e5a69ad2f134603291319519b3440d5
Gustavo Poppi <gustavopoppi@hotmail.com>
2021-05-02 21:11:45 -0300

    Removendo comentários

--- src/main/java/br/com/loja/mvc/sergio/comuns/Constant.java
+++ src/main/java/br/com/loja/mvc/sergio/comuns/Constant.java
@@ -4,8 +4,4 @@ public class Constant {
 
 	public static final String QUERY_DATA_ENTRE_MES_ATUAL = "  AND STR_TO_DATE(P.dataParcela , '%d/%m/%Y') >= STR_TO_DATE(:dataPrimeiroDiaMes , '%Y/%m/%d')"
 			+ "  AND STR_TO_DATE(P.dataParcela , '%d/%m/%Y') <= STR_TO_DATE(:dataUltimoDiaMes , '%Y/%m/%d')";
-	
-	
-//	+ "  AND STR_TO_DATE(P.dataParcela , '%d/%m/%Y') >= STR_TO_DATE(:dataPrimeiroDiaMes , '%Y/%m/%d')"
-//	 + "  AND STR_TO_DATE(P.dataParcela , '%d/%m/%Y') <= STR_TO_DATE(:dataUltimoDiaMes , '%Y/%m/%d')"
 }
